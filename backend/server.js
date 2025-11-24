8dc12f74ea9e831f80a1218ed44bed6d8d48bb52
MwebembeziNicole <nicolemwebembezi18@gmail.com>
2025-11-24 15:40:06 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { connectDB } from './config/db.js'
 
+import Uploadrouter from './routes/upload.routes.js'
+import Eventrouter from './routes/events.routes.js'
+import { urlencoded } from 'express'
+
 dotenv.config()
 
 const app = express()
@@ -11,8 +15,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
+app.use(urlencoded({ extended: true }))
 
 const port = process.env.PORT || 5000
+
+// api connections
+app.use('/api', Uploadrouter)
+app.use('/api', Eventrouter)
 
 // Connect DB first, then start server
 connectDB().then(() => {
