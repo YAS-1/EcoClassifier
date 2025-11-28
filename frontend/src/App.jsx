35e6364a4cbd6f03393ef2258c8a62df0fed01c1
Denise Priscila <denisepriscilamuwanguzi@gmail.com>
2025-11-28 16:54:30 +0300

    Updates to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,10 +12,10 @@ function App() {
         <Route path="/upload" element={<UploadPage />} />
         <Route path="/events" element={<EventsPage />} />
         {/* placeholder routes for future pages */}
-        <Route path="/analytics" element={<div>Analytics - TODO</div>} />
-        <Route path="/models" element={<div>Models - TODO</div>} />
-        <Route path="/settings" element={<div>Settings - TODO</div>} />
-        <Route path="/help" element={<div>Help - TODO</div>} />
+        <Route path="/analytics" element={<div>Analytics - TO DO</div>} />
+        <Route path="/models" element={<div>Models - TO DO</div>} />
+        <Route path="/settings" element={<div>Settings - TO DO</div>} />
+        <Route path="/help" element={<div>Help - TO DO</div>} />
       </Routes>
     </Layout>
   );
